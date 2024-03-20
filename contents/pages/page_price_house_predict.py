84b606fe3d30b2b2b60f7d347727189f80a94fc6
Lesha <aleksvkv95@gmail.com>
2024-03-20 04:36:16 +0300

    add some features for predict dataet in web site

--- contents/pages/page_price_house_predict.py
+++ contents/pages/page_price_house_predict.py
@@ -7,19 +7,31 @@ st.set_page_config(
 st.sidebar.success("Выберете интересующий раздел")
 st.title('Оценка стоимости недвижимости')
 st.subheader('Выберите параметры квартиры, значения которых вы знаете')
-df = pd.DataFrame(columns=['total_area', 'floor', 'floors_number', 'house_age',
-       'living_area', 'kitchen_area', 'longitude', 'latitude', 'mean_metro',
-       'metro_dist', 'metro_transport_time', 'region', 'rooms_count',
-       'is_complete', 'house_material', 'parking', 'passenger_elevator',
-       'is_apartments', 'is_auction', 'is_first_floor', 'is_last_floor',
-       'has_metro'])
-options = st.multiselect("Признаки",['Общая площадь', 'Номер этажа', 'Количество этажей', 'Возраст дома', 'Жилая_площадь', 'Площадь_кухни', 'Долгота_квартиры', 'Широта_квартиры', 'Среднее_расстояние_до_метро', 'Расстояние_до_метро', 'Время_транспортировки_до_метро', 'Регион', 'Количество_комнат', 'Готовность', 'Материал_дома', 'Парковка', 'Пассажирский_лифт', 'Это_апартаменты', 'Аукцион', 'Первый_этаж', 'Последний_этаж', 'Наличие_метро'])
+df = pd.DataFrame(columns=['region', 'address', 'total_area', 'kitchen_area',
+       'living_area', 'rooms_count', 'floor', 'floors_number', 'build_date',
+       'is_complete', 'completion_year', 'house_material', 'parking',
+       'decoration', 'balcony', 'longitude', 'latitude', 'passenger_elevator',
+       'cargo_elevator', 'metro', 'metro_distance', 'metro_transport',
+       'district', 'is_apartments', 'is_auction'])
+options = st.multiselect("Признаки",['Регион', 'Адрес', 'Общая площадь', 'Площадь кухни', 'Жилая площадь', 'Количество комнат', 'Номер этажа', 'Количество этажей', 'Год начала строительства дома', 'Дом готов', 'Год завершения строительства дома', 'Материал дома', 'Тип парковки', 'Тип отделки', 'Число балконов', 'Долгота дома', 'Широта дома', 'Число пассажирских лифтов', 'Число грузовых лифтов', 'Ближайшие станции метро', 'Расстояния до метро', 'Тип перемещения до метро', 'Район', 'Квартира - апартаменты?', 'Квартира на аукционе?'])
 
 for option in options:
-    if option == 'Общая площадь':
-        total_area = st.number_input('Площадь дома в кв. метрах', value=None, min_value=5.0, max_value=2000.0, step=0.1,
+    if option == 'Регион':
+        region = st.radio('Область расположения квартиры:', ['spb', 'msk',  'kzn',  'nng', 'ekb', 'nsk'])
+    elif option == 'Адрес':
+        address = st.text_area('Адрес дома:')
+    elif option == 'Общая площадь':
+        total_area = st.number_input('Площадь дома в кв. метрах:', value=None, min_value=5.0, max_value=2000.0, step=0.1,
+                                     placeholder='Введите число...')
+    elif option == 'Площадь кухни':
+        total_area = st.number_input('Площадь кухни в кв. метрах:', value=None, min_value=5.0, max_value=2000.0, step=0.1,
+                                     placeholder='Введите число...')
+    elif option == 'Жилая площадь':
+        total_area = st.number_input('Жилая площадь в кв. метрах:', value=None, min_value=5.0, max_value=2000.0, step=0.1,
                                      placeholder='Введите число...')
     elif option == 'Номер этажа':
         floor = st.slider('Номер этажа квартиры', 1, 100)
     elif option == 'Количество этажей':
         floors_number = st.slider('Количество этажей в квартире', 1, 100)
+
+
